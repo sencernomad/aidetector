26dd3ccec8e6e37d68e24cda316cfc0cafca54f1
Arden <aolgundemir@gmail.com>
2025-07-08 16:37:20 +0300

    Stripe hard code added

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -3,11 +3,11 @@ import Stripe from 'stripe';
 
 // Frontend Stripe instance
 export const getStripe = () => {
-  const stripePublishableKey = process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!;
+  const stripePublishableKey = "pk_live_51QcmHGRv8iAshBPv3paps9LK55bARyH68V9Ws1Qs4xe6DhBYYrcZBTb25bknUKiIx8wInKKRVbgxMpKmRQLvxndx00N6ncZchM";
   return loadStripe(stripePublishableKey);
 };
 
 // Backend Stripe instance
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+export const stripe = new Stripe("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", {
   apiVersion: '2025-05-28.basil', // Use the latest API version
 }); 
